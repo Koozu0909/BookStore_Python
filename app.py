c6fdc15bdb4ba664f0ea862059fb189829755ad0
michaelhung123 <zuroken140@gmail.com>
2022-11-28 22:05:01 +0700

    update code html css

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run()
